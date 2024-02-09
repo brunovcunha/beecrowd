4487ab14d2583eab6a946faa73236f2cda2744af
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-02-08 22:15:17 -0300

    modified:   1001/src/App.java

--- 1001/src/App.java
+++ 1001/src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int x = a + b;
+        System.out.println("X = " + x);
+
+        s.close();
     }
 }
