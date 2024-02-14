b40fe75e245edf9c8fe2b3fbb80d44cdb1587bb4
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-02-13 22:35:55 -0300

    new file:   1004/.vscode/settings.json #	new file:   1004/README.md #	new file:   1004/bin/App.class #	new file:   1004/src/App.java

---
+++ 1004/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+
+        System.out.println("PROD = " + (n1 * n2));
+    }
+}
