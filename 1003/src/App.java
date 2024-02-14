a00e1c8c3428983a23d8620e2396bbe8e4d39706
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-02-13 22:30:10 -0300

    modified:   1003/src/App.java

--- 1003/src/App.java
+++ 1003/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner s =  new Scanner(System.in);
+
+       int n1 = s.nextInt();
+       int n2 = s.nextInt();
+
+       System.out.println("SOMA = " + (n1 + n2));
+
+       s.close();
     }
 }
